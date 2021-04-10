38b5f75568421dcbc86987a2417bc8bb8685c943
Ricky Felix <rickyfelix19@outlook.com>
2021-04-10 19:18:02 +1000

    add lab files

---
+++ Labs/lab 1/tutor/sample/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>My first page</title>
+</head>
+<body>
+    <h1>Hello World! This is my first web page!!</h1>
+    <p>My name is Cindy!</p>
+</body>
+</html>
