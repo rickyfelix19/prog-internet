b5def2c55a80280421ce64a1f88222295759a1b9
Ricky Felix <rickyfelix19@outlook.com>
2021-04-13 16:39:52 +1000

    link other php pages to index

--- asg-1/pages/index.php
+++ asg-1/pages/index.php
@@ -18,8 +18,10 @@
         <main>
             <h2>Online Grocery</h2>
                 <?php include 'left-hand.php';?>
+                <!-- <?php include 'top-right.php';?> -->
+                <!-- <?php include 'bottom-right.php';?> -->
         </main>
         
-        <script src="../js/script.js"></script>
+        <!-- <script src="../js/script.js"></script> -->
     </body>
 </html>
