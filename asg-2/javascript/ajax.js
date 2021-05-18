7724b292b064367fc3c763eb127b1e6df5e9d17c
Ricky Felix <rickyfelix19@outlook.com>
2021-05-19 02:52:07 +1000

    change boilerplate

---
+++ asg-2/javascript/ajax.js
@@ -0,0 +1,13 @@
+// function showFilms() {
+  
+//  //add the missing code to get the films title from dbcode.php and add them to select html "films" using innerHTML
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (xhttp.readyState == 4 && xhttp.status == 200) {
+//       document.getElementById("films").innerHTML = xhttp.responseText;
+//     }
+//   };
+//  //look to the previous exercise Ex2.html
+//   xhttp.open("GET", "dbcode.php", true);
+//   xhttp.send();
+// }
